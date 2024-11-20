93c6facbc7686033dedea8b888858a6aeea58a0a
KarpPlaysLive <yuchen2020@hotmail.com>
2024-11-19 17:13:51 -0800

    Added 'code' folder and VEXdrive.py file

---
+++ code/VEXdrive.py
@@ -0,0 +1,9 @@
+while (true) {
+  speed = Controller1.Axis3.position();
+  turning = Controller1.Axis1.position();
+  Drivetrain.setTurnVelocity(fabs(static_cast<float>(turning)), percent);
+  Drivetrain.setDriveVelocity(fabs(static_cast<float>(speed)), percent);
+  Drivetrain.drive(forward);
+  wait(20, msec);
+  }
+  return 0;
